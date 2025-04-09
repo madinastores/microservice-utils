19ede8ea0685df06a274da50a7b68e3a95d18e51
abdallahnasar <abdallahnasar@gmail.com>
2025-04-09 19:12:22 +0200

    Initial commit

---
+++ src/MadinastoresMicroserviceUtilsServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Madinastores\MadinastoresMicroserviceUtils;
+
+use Illuminate\Support\ServiceProvider;
+
+class MadinastoresMicroserviceUtilsServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        // You can bind services here
+    }
+
+    public function boot()
+    {
+        // Load routes, migrations, views, etc. if needed
+    }
+}
